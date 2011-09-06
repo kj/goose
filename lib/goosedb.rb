d41a8a8aab869e49f78ef59539bb225a79a28fc2
Kelsey Judson <kelseyjudson@gmail.com>
2011-09-07 04:51:26 +1200

    Added redis configuration file to switch database etc.

--- lib/goosedb.rb
+++ lib/goosedb.rb
@@ -1,14 +1,17 @@
 require 'redis'
+require 'yaml'
 
 class GooseDB
   def initialize
+    @config = YAML.load_file './config/redis.yml'
+
     @goose_db = Redis.new
-    @goose_db.select '3'
+    @goose_db.select @config['db_number']
   end
 
   def setup_check
     print "Goose will now set up your redis database for first time use.\n"
-    print "This will wipe redis database 3!!! Continue? [y/n] " 
+    print "This will wipe redis database #{@config['db_number']}!!! Continue? [y/n] " 
     answer = gets.chomp
 
     if answer == "y"
@@ -19,7 +22,7 @@ class GooseDB
   end
 
   def first_time_setup
-    puts "Wiping database 3 and setting up initial keys."
+    puts "Wiping database #{@config['db_number']} and setting up initial keys."
 
     @goose_db.flushdb
     @goose_db.set 'player', '0' # Used for setting player ids in key format "player:id:attribute"
